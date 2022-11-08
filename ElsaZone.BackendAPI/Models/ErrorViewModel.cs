8e75a4b6798a95cd4435135851f2b614eb65807b
Frozen Queen Elsa <bebebietyeu0903@gmail.com>
2022-11-08 10:12:18 +0700

    Create Solution Structure

---
+++ ElsaZone.BackendAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ElsaZone.BackendAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
